9552c3d784db0e46f95d761f16b014c0f007408b
Abdel <77060081+abdulrashid232@users.noreply.github.com>
2024-02-20 10:13:16 +0000

    added the json file, and fetch the data & pass it to the jobComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'devjobs';
+  jobs: any;
+
+  
 }
--- src/app/jobs/jobs.component.ts
+++ src/app/jobs/jobs.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-jobs',
   templateUrl: './jobs.component.html',
   styleUrl: './jobs.component.css'
 })
-export class JobsComponent {
+export class JobsComponent implements OnInit{
+  jobs:any; 
 
+  constructor() { }
+
+  ngOnInit() {
+    this.loadJobs();
+  }
+
+  loadJobs() {
+    fetch('./assets/data.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(jobs => {
+      this.jobs = jobs;
+    })
+    .catch(error => {
+      console.error('Error While fetching data:', error);
+    });
+  }
 }
