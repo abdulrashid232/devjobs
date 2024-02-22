7d675c72a8b24468ef251e40d6a88841f8c23300
Abdel <77060081+abdulrashid232@users.noreply.github.com>
2024-02-22 14:34:04 +0000

    update the button hover effects

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -8,7 +8,6 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class ButtonComponent {
   constructor() { }
  @Input() label: string = '';
- @Input() style: string = '';
  @Output() onClick:EventEmitter<void> = new EventEmitter<void>();
 
 
