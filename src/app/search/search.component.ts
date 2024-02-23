379b436f300800f50583d7b8944512762ea2cd1a
Abdel <77060081+abdulrashid232@users.noreply.github.com>
2024-02-23 19:09:34 +0000

    add dark theme to the search component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
-export class SearchBarComponent {
+export class SearchBarComponent implements OnInit{
 
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.theme$.subscribe((isDark: boolean) => {
+    
+      const div = document.querySelectorAll('.darkContainer')
+      div.forEach((item) => {
+        item.classList.toggle('dark-theme', isDark);
+      });
+
+    });
+  }
+  search: string = '';
+  searchJobs() {
+    console.log(this.search);
+  }
+  clearSearch() {
+    this.search = '';
+  }
 }
